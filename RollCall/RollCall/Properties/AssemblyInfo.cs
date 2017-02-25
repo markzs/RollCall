abea8018920aaf93bd09495a6d7e9c45441b2843
mark <mark@DESKTOP-1G11LM4>
2017-02-25 11:47:05 -0600

    RSVP app is working as intended.

--- RollCall/RollCall/Properties/AssemblyInfo.cs
+++ RollCall/RollCall/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8a342c19-4ff5-45d8-b893-2512fa0c90d7")]
+[assembly: Guid("c1754f37-1a6e-48a7-a9f3-24cb055e662e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
